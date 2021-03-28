450ed5c80ec8772a290da84286ed0915533734c0
Beeno Tung <aabbcc1241@yahoo.com.hk>
2021-03-29 00:01:17 +0800

    lint fix

--- booking-ng/src/app/app.component.ts
+++ booking-ng/src/app/app.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core'
-import { binArray } from '@beenotung/tslib/array'
+import { Component } from '@angular/core';
+import { binArray } from '@beenotung/tslib/array';
 
 type Day = {
   day: number
   currentMonth: boolean
   selected?: boolean
   disabled?: boolean
-}
+};
 
 @Component({
   selector: 'app-root',
@@ -14,32 +14,32 @@ type Day = {
   styleUrls: ['./app.component.sass'],
 })
 export class AppComponent {
-  title = 'booking-ng'
-  weekDays = `SMTWTFS`.split('')
-  days: Day[] = []
+  title = 'booking-ng';
+  weekDays = `SMTWTFS`.split('');
+  days: Day[] = [];
 
   constructor() {
-    this.days.push({ day: 28, currentMonth: false })
+    this.days.push({ day: 28, currentMonth: false });
     for (let i = 1; i <= 31; i++) {
       this.days.push({
         day: i,
         currentMonth: true,
         disabled: i >= 15 && i <= 19,
-      })
+      });
     }
     for (let i = 1; i <= 10; i++) {
-      this.days.push({ day: i, currentMonth: false })
+      this.days.push({ day: i, currentMonth: false });
     }
   }
 
-  getDayWeeks() {
-    return binArray(this.days, 7)
+  getDayWeeks(): Day[][] {
+    return binArray(this.days, 7);
   }
 
-  selectDay(day: Day) {
+  selectDay(day: Day): void {
     if (day.disabled) {
-      return
+      return;
     }
-    day.selected = true
+    day.selected = true;
   }
 }
